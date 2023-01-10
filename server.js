b66740abfd4c47c60b143ea9a31dcf4d68d3f3f8
buloko <buloko8@gmail.com>
2023-01-10 09:39:03 -0500

    installiing console and sql2.

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        password: ''.
+       
+        
+    }
+)
