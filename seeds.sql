275a69eb3c60fd0ae2b8ed16e2ec53ddf31ff437
buloko <buloko8@gmail.com>
2023-01-10 18:06:13 -0500

    department names

--- seeds.sql
+++ seeds.sql
@@ -1,1 +1,7 @@
-INSERT INTO 
+INSERT INTO department (name)
+VALUES("Administration/operations"),
+("Research and Devlopment"),
+("Marketing and sales"),
+("Human resources"),
+("Customer service"),
+("Accounting and finance")
