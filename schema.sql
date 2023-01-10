6b48bebe82feead94d9df3083d20afe3f74ca49a
buloko <buloko8@gmail.com>
2023-01-10 13:35:50 -0500

    added the foreigin keys into schema

--- schema.sql
+++ schema.sql
@@ -3,18 +3,25 @@ CREATE DATABASE company_db;
 
 USE company_db
 
-CREATE TABLE department
+CREATE TABLE department (
 id INT PRIMARY KEY
 name: VARCHAR(30)
+):
 
-CREATE TABLE role
+CREATE TABLE roles(
+
 id: INT PRIMARY KEY
 title: VARCHAR(30)
 salary: DECIMAL
 department_id: INT
+FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
 
-CREATE TABLE employee
+CREATE TABLE employees (
 first_name: VARCHAR(30)
 last_name: VARCHAR(30)
 role_id:INT
 manager_id:INT, IF no manager_id NULL
+FOREIGN KEY (role_id) REFERENCES roles(role_id)
+FOREIGN KEY (manager_id) REFERENCES employees(employee_id)
+);
