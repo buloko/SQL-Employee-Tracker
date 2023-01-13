9e1f3ce3e9c997fa3217c365c4fa3bb2a53aa5dc
buloko <buloko8@gmail.com>
2023-01-12 19:43:09 -0500

    adding seed data for roles

--- seeds.sql
+++ seeds.sql
@@ -1,7 +1,24 @@
 INSERT INTO department (name)
 VALUES("Administration/operations"),
 ("Research and Devlopment"),
-("Marketing and sales"),
+("Marketing and Sales"),
 ("Human resources"),
-("Customer service"),
-("Accounting and finance")
+("Legal"),
+("Accounting and Finance")
+
+INSERT INTO roles (title,salary, department_id,)
+VALUES("Chief Executive Officer",800000, 1),
+("Chief Operating Officer," 400000, 2),
+("Chief Technology Officer," 300000, 3),
+("President",100000,4),
+("Executive Assistant", 82000, 5),
+("Market Manager", 140000, 6),
+("Product Manager", 120000, 7),
+("Project Manger", 100000, 8),
+("Marketing Specialist", 70000,9),
+("Accountant" 60000,10),
+("Sales Representative",70000,11 )
+("Customer Service Representative",50000,12)
+
+
+INSERT
