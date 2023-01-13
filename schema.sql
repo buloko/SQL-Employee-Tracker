8bbaf8405a3e34c9f0f56dfd0dd58ee70d8c3cfa
buloko <buloko8@gmail.com>
2023-01-12 22:12:38 -0500

    syntax fixed and seeds data added

--- schema.sql
+++ schema.sql
@@ -14,7 +14,7 @@ id: INT PRIMARY KEY
 title: VARCHAR(30)
 salary: DECIMAL
 department_id: INT
-FOREIGN KEY (department_id) REFERENCES departments(department_id)
+FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
@@ -22,6 +22,6 @@ first_name: VARCHAR(30)
 last_name: VARCHAR(30)
 role_id:INT
 manager_id:INT,NULL
-FOREIGN KEY (role_id) REFERENCES roles(role_id)
-FOREIGN KEY (manager_id) REFERENCES employees(employees(id))
+FOREIGN KEY (role_id) REFERENCES roles(id)
+FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
--- seeds.sql
+++ seeds.sql
@@ -9,17 +9,30 @@ VALUES("Administration/operations"),
 INSERT INTO roles (title,salary, department_id,)
 VALUES("Chief Executive Officer",800000, 1),
 ("Chief Operating Officer",400000, 2),
-("Chief Technology Officer",300000, 3),
-("President",100000,4),
-("Executive Assistant", 82000, 5),
-("Market Manager", 140000, 6),
-("Product Manager", 120000, 7),
-("Project Manger", 100000, 8),
-("Marketing Specialist", 70000,9),
-("Accountant" 60000,10),
-("Sales Representative",70000,11 )
+("Chief Information Ofiicer",300000,3)
+("Chief Technology Officer",300000, 4),
+("President",100000,5),
+("Executive Assistant", 82000, 6),
+("Market Manager", 140000, 7),
+("Product Manager", 120000, 8),
+("Project Manger", 100000, 9),
+("Marketing Specialist", 70000,10),
+("Accountant" 60000,11),
+("Sales Representative",70000,12),
 ("Customer Service Representative",50000,12)
 
 
 INSERT INTO employees (first_name,last_name,role_id,manager_id)
-VALUES()
+VALUES("Mattie", "Rhea", 1, NULL),
+("Leighton","Jeana", 2, 1),
+("Lance","Vera", 3, 1 ),
+("Brannon", "Gaz", 4, 3),
+("Garret", "Ericka", 5, NULL),
+("Maleah","Conner", 6,5 ),
+("Tamsen", "Jensen",7,1),
+("Adella", "Dene", 8, 2),
+("Orrell" "Deirdre", 9, 2),
+("Christiana", "Willard", 10,7),
+("Clarence", "Baxter", 11, 2),
+("Liberty", "Breanna", 12,1 ),
+("Emery", "Stacia", 13, 1),
