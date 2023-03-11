990b8369d21939add4a51707414ef7bb29c7ca0c
buloko <buloko8@gmail.com>
2023-03-10 22:33:08 -0500

    fixing syntax errors

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,23 +5,24 @@ VALUES
 ("Marketing and Sales"),
 ("Human resources"),
 ("Legal"),
-("Accounting and Finance")
+("Accounting and Finance");
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-("Chief Executive Officer",800000, 1),
-("Chief Operating Officer",400000, 2),
-("Chief Information Officer",300000,3)
-("Chief Technology Officer",300000, 4),
-("President",100000,5),
-("Executive Assistant", 82000, 6),
-("Market Manager", 140000, 7),
-("Product Manager", 120000, 8),
-("Project Manger", 100000, 9),
+("Chief Executive Officer", 800000, 1),
+("Chief Operating Officer", 400000, 2),
+("Chief Information Officer", 300000, 3),
+("Chief Technology Officer", 300000, 4),
+("President", 100000,5),
+("Executive Assistant", 82000,6),
+("Market Manager", 140000,7),
+("Product Manager", 120000,8),
+("Project Manager", 100000,9),
 ("Marketing Specialist", 70000,10),
-("Accountant" 60000,11),
+("Accountant", 60000,11),
 ("Sales Representative",70000,12),
-("Customer Service Representative",50000,13)
+("Customer Service Representative",50000,13);
+
 
 
 INSERT INTO employees (first_name,last_name,role_id,manager_id)
@@ -34,8 +35,8 @@ VALUES
 ("Maleah","Conner", 6,5 ),
 ("Tamsen", "Jensen",7,1),
 ("Adella", "Dene", 8, 2),
-("Orrell" "Deirdre", 9, 2),
+("Orrell","Deirdre", 9, 2),
 ("Christiana", "Willard", 10,7),
 ("Clarence", "Baxter", 11, 2),
 ("Liberty", "Breanna", 12,1 ),
-("Emery", "Stacia", 13, 1),
+("Emery", "Stacia", 13, 1);
--- index.js
+++ index.js
@@ -1,134 +1,7 @@
 const inquirer = require("inquirer");
 const db = require('./connection');
 
-// function to view all employees
-function viewEmploy() {
-  db.query("SELECT * FROM employee", (err, res) => {
-    if (err) throw err;
-    console.table(res);
-    startPrompt();
-  });
-}
-//function to view all departments
-const viewDepart = () => {
-  db.query("SELECT * FROM department", (err, res) => {
-    if (err) throw err;
-    console.table(res);
-    startPrompt();
-  });
-};
-const viewRoles = () => {
-  db.query("SELECT * FROM roles", (err, res) => {
-    if (err) throw err;
-    console.table(res);
-    startPrompt();
-  });
-};
 
-// Add a new employee
-const newEmploy = () => {
-  db.query("SELECT * FROM employee", (err, res) => {
-    if (err) throw err;
-    inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: "firstName",
-          message: "What is the employee's first name?",
-        },
-        {
-          type: 'input',
-          name: "lastName",
-          message: "What is the employee's last name?",
-        },
-        {
-          type: 'list',
-          name: "role",
-          message: "What is the employee's role?",
-          choices:res.map(role => role.title)
-        },
-        {
-          type: 'list',
-          name: 'managerId',
-          message: 'What is the ID of the manager?',
-          choices: [1,2,3,5,7]
-        },
-      ])
-      .then((data) => {
-        let roleTitle = res.find((role) => role.title === data.role);
-        db.query("INSERT INTO employee SET ?"),
-          {
-            first_name: data.firstName,
-            last_name: data.lastName,
-            role_id: roleTitle.id,
-            manager_id: data.managerId,
-          };
-      });
-
-    startPrompt();
-  });
-};
-// Add a role
-const newRole = () => {
-    db.query ('SELECT * FROM department', (err,res) => {
-        if(err) throw err
-        inquirer.prompt([
-            {
-                type:'input',
-                name: 'title',
-                message: 'What is the name of the role'
-            },
-            {
-                type:'input',
-                name:'salary',
-                message:'What is the salary of this role'
-            },
-            {
-                type: 'list',
-                name: 'department',
-                message: 'Which department does the role belong to?',
-                choices: res.map(department => department.name)
-            }
-        ]) .then (data => {
-            let deptName = res.find(department => department.name === data.department)
-            db.query ('INSERT INTO role SET ?', {
-                title: data.title,
-                salary: data.salary,
-                department_id: deptName.id,
-            })
-
-            startPrompt()
-
-        })
-
-    })
-
-};
-// Add a department 
-const newDept = () => {
-    db.query('SELECT * FROM department',(err,res) => {
-    if(err) throw err
-    inquirer.prompt([
-        {
-            type: 'input',
-            name:'name',
-            message:'What is the new Department called?'
-        },
-    ])  .then (data => {
-        db.query ('INSERT INTO department SET ?', {
-            name: data.name,
-        })
-    })
-    startPrompt()
-    })
-
-    
-};
-       
-    
-
-            
-    
 function startPrompt() {
   inquirer
     .prompt([
@@ -177,6 +50,134 @@ function startPrompt() {
       }
     });
 }
+// function to view all employees
+function viewEmploy() {
+  db.query("SELECT * FROM employee", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    startPrompt();
+  });
+}
+//function to view all departments
+const viewDepart = () => {
+  db.query("SELECT * FROM department", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    startPrompt();
+  });
+};
+const viewRoles = () => {
+  db.query("SELECT * FROM roles", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    startPrompt();
+  });
+};
+
+// Add a new employee
+const createEmploy = () => {
+  db.query("SELECT * FROM employee", (err, res) => {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: "firstName",
+          message: "What is the employee's first name?",
+        },
+        {
+          type: 'input',
+          name: "lastName",
+          message: "What is the employee's last name?",
+        },
+        {
+          type: 'list',
+          name: "role",
+          message: "What is the employee's role?",
+          choices:res.map(role => role.title)
+        },
+        {
+          type: 'list',
+          name: 'managerId',
+          message: 'What is the ID of the manager?',
+          choices: [1,2,3,5,7]
+        },
+      ])
+      .then((data) => {
+        let roleTitle = res.find((role) => role.title === data.role);
+        db.query("INSERT INTO employee SET ?"),
+          {
+            first_name: data.firstName,
+            last_name: data.lastName,
+            role_id: roleTitle.id,
+            manager_id: data.managerId,
+          };
+      });
+
+    startPrompt();
+  });
+};
+// Add a role
+const createRol = () => {
+    db.query ('SELECT * FROM department', (err,res) => {
+        if(err) throw err
+        inquirer.prompt([
+            {
+                type:'input',
+                name: 'title',
+                message: 'What is the name of the role'
+            },
+            {
+                type:'input',
+                name:'salary',
+                message:'What is the salary of this role'
+            },
+            {
+                type: 'list',
+                name: 'department',
+                message: 'Which department does the role belong to?',
+                choices: res.map(department => department.name)
+            }
+        ]) .then (data => {
+            let deptName = res.find(department => department.name === data.department)
+            db.query ('INSERT INTO role SET ?', {
+                title: data.title,
+                salary: data.salary,
+                department_id: deptName.id,
+            })
+
+            startPrompt()
+
+        })
+
+    })
+
+};
+// Add a department 
+const createDepart = () => {
+    db.query('SELECT * FROM department',(err,res) => {
+    if(err) throw err
+    inquirer.prompt([
+        {
+            type: 'input',
+            name:'name',
+            message:'What is the new Department called?'
+        },
+    ])  .then (data => {
+        db.query ('INSERT INTO department SET ?', {
+            name: data.name,
+        })
+    })
+    startPrompt()
+    })
+
+    
+};
+       
+    
+
+            
+    
 
 startPrompt();
 
